ebee46635438577617a50afbd9ea8ea9f735e336
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2024-02-04 22:13:23 +0530

    "making things work"

--- app.py
+++ app.py
@@ -33,9 +33,44 @@ if uploaded_file:
     data = loader.load()
     st.json(data)
     embeddings = HuggingFaceEmbeddings(
-        model_name = 'sentence-transformers/all-MiniLM-L6-v2'
-
+        model_name = 'sentence-transformers/all-MiniLM-L6-v2',
+        model_kwargs = {'device': 'cpu'}
     )
 
     db = faiss.from_documents(data, embeddings)
     db.save_local(DB_FAISS_PATH)
+
+    llm = load_llm()
+    chain = ConversationalRetrievalChain.from_llm(llm = llm, retriever=db.as_retriever())
+
+    def conversational_chat(query):
+        result = chain({"question": query, "chat_history":st.session_state['history']})
+        st.session_state['history'].append((query, result["answer"]))
+        return result["answer"]
+
+        if 'generated' not in st.session_state:
+            st.session_state['generated'] = ["Hello ! Ask me anything about " + uploaded_file.name]
+        
+        if 'past' not in st.session_state:
+            st.session_state['past'] = ["Hey !"]
+        
+        response_container = st.container()
+        container = st.container()
+
+        with container:
+            with st.form(key='my_form', clear_on_submit=True):
+
+                user_input = st.text_input("Query:", placeholder="Talk to your csv data here (:", key='input')
+                submit_button = st.form_submit_button(label='Send')
+
+            if submit_button and user_input:
+                output = conversational_chat(user_input)
+
+                st.session_state['past'].append(user_input)
+                st.session_state['generated'].append(output)
+
+        if st.session_state['generated']:
+            with response_container:
+                for i in range(len(st.session_state['generated'])):
+                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
+                    message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
