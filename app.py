67beab711e7d13cdd3ce49b2c72d380136412052
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2024-02-08 23:04:32 +0530

    ",....,"

--- app.py
+++ app.py
@@ -75,3 +75,4 @@ if uploaded_file :
             for i in range(len(st.session_state['generated'])):
                 message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
                 message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+                
